45dd42c979eda8a168608153903552e616ec7440
Brooke <brookewu2023@gmail.com>
2024-12-25 11:50:03 -0500

    Complete exercise 02 repeatString

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function(string, number) {
+    let result = "";
+    // Edge case: number is negative
+    if (number < 0) {
+        return "ERROR";
+    }
+    for (let i = 0; i < number; i++) {
+        result += string;
+    }    
+    return result;
 };
 
 // Do not edit below this line
